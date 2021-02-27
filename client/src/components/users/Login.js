4988629088f2307f0a1cd6ac7e8ec7019b1a9531
fatsoap <g8sigma@icloud.com>
2021-02-27 21:33:38 +0800

    commit before change

--- client/src/components/users/Login.js
+++ client/src/components/users/Login.js
@@ -2,6 +2,8 @@ import React, { useEffect } from 'react';
 import axios from 'axios';
 import  { Redirect, Route, Link } from 'react-router-dom'
 import Header from '../Header';
+import SimpleReactValidator from 'simple-react-validator';
+
 
 
 const Login =  () => {
@@ -10,21 +12,27 @@ const Login =  () => {
     const [loggedin, setLoggedin] = React.useState(false);
     const [errormsg, setErrormsg] = React.useState('');
 
-    // useEffect (() => {
-    //     axios.get('/users/login').then(res=>console.log(res));
-    // });
+    const Validator = new SimpleReactValidator();
+    const [valiError, setValiError] = React.useState(false);
+
+
 
     const loginSubmit = (e) => {
         e.preventDefault();
-        //console.log({username});
-        //console.log({password});
         const user = {username: username, password: password};
-        axios.post('/users/login', user)
-            .then((res) => {
-                //console.log(res.data);
-                setLoggedin(res.data.success);
-                setErrormsg(res.data.message);
-            })
+        if(Validator.allValid()){
+            console.log('all done');
+            setValiError(false);
+        } else {
+            Validator.showMessages();
+      
+        }
+        // axios.post('/users/login', user)
+        //     .then((res) => {
+        //         //console.log(res.data);
+        //         setLoggedin(res.data.success);
+        //         setErrormsg(res.data.message);
+        //     })
     }
 
     
@@ -41,6 +49,8 @@ const Login =  () => {
                 <div className="login__form">
                     <form onSubmit={loginSubmit} >
                         <input type="text" name="username" value={username} onChange={e=>setUsername(e.target.value)} placeholder="username" />
+                        {valiError? <div>www</div>: <div></div>}
+                        {Validator.message('username', username, 'required|email')}
                         <input type="password"  name="password" value={password} onChange={e=>setPassword(e.target.value)} placeholder="password" />
                         <button type="submit" value="submit">Login</button>
                     </form>
