db46ecbd61be411fcbc9a59734b37639c60af405
fatsoap <g8sigma@icloud.com>
2021-02-28 23:57:09 +0800

    remove node_modules with gitignore

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -5,37 +5,48 @@ import axios from 'axios';
 const Header = ({loggedin}) => {
 
     return(
-        <div className="header" style={{backgroundColor:'lightblue'}}>
-            <div className="header__logo">
+        <div className="navbar navbar-expand-lg navbar-light bg-light" >
+            <div className="container-fluid"> 
+            <div className="navbar-brand">
                 <a href="/"><img alt="logo" src={logo} /></a>
+            </div>
+            <div class="collapse navbar-collapse">
+                <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item"><a className="nav-link active" href="/dashboard">dashboard</a></li>
+                    <li class="nav-item"><a href="/users/register">register</a></li>
+                    <li class="nav-item"><a href="/users/login">login</a></li>
+                    <li class="nav-item"><a href="/users/logout">logout</a></li>
+                    <li class="nav-item"></li>
+                </ul>
             </div>
             <Dashboard />
             {loggedin? <div><Logout /> </div>:<div><Login /><Register /></div> }
+            </div>
         </div>
     );
 }
 
 const Dashboard = () => {
     return(
-        <div className="header__dashboard"><a href="/dashboard">dashboard</a></div>
+        <div className="header__dashboard"></div>
     );
 }
 
 const Register = () => {
     return(
-        <div className="header__register"><a href="/users/register">register</a></div>
+        <div className="header__register"></div>
     );
 }
 
 const Login = () => {
     return(
-        <div className="header__login"><a href="/users/login">login</a></div>
+        <div className="header__login"></div>
     );
 }
 
 const Logout = () => {
     return(
-        <div className="header__logout"><a href="/users/logout">logout</a></div>
+        <div className="header__logout"></div>
     );
 }
 
